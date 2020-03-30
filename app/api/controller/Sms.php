8b1858f32e8e88954bb884e21d5393af73a968dc
钟浩彬 <chum3@qq.com>
2020-03-30 22:38:06 +0800

    7-6

--- app/api/controller/Sms.php
+++ app/api/controller/Sms.php
@@ -9,10 +9,26 @@ declare(strict_types=1);
 namespace app\api\controller;
 
 use app\BaseController;
+use app\common\business\Sms as SmsBus;
 
 class Sms extends BaseController {
 
     public function code() :object {
-        return show(config("status.success"),"OK");
+        $phoneNumber = input('param.phone_number','','trim');
+
+        $data = [
+            'phone_number' => $phoneNumber,
+        ];
+        try {
+            validate(\app\api\validate\User::class)->scene("send_code")->check($data);
+        }catch (\think\exception\ValidateException $e) {
+            return show(config("status.error"), $e->getError());
+        }
+
+        // 调用business层的数据
+        if(SmsBus::sendCode($phoneNumber)) {
+            return show(config("status.success"),"发送验证码成功");
+        }
+       return show(config("status.success"),"发送验证码失败");
     }
 }
--- app/common/business/Sms.php
+++ app/common/business/Sms.php
@@ -7,9 +7,17 @@
  */
 declare(strict_types=1);
 namespace app\common\business;
-
+use app\commom\lib\sms\AliSms;
 class Sms {
-    public static function sendCode() :bool {
+    public static function sendCode(string $phoneNumber) :bool {
+
+        $code = rand(100000,999999);
+        $sms = AliSms::sendCode($phoneNumber,$code);
+        if($sms) {
+            // todo 把短信验证码记录到redis，并且给出一个失效时间 1min有效
+            cache(config("redis.code_pre").$phoneNumber,$code,config("redis.code_expire"));
+        }
+
         return true;
     }
 }
